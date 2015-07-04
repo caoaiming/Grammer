f4730c35bbb2894f2ac0965e2310499a10040f2f
caoaiming <502253145@qq.com>
2015-07-04 12:57:26 +0800

    计算两个时间之间的间隔(以天为单位)

--- Boost/boost_calculate_date.cpp
+++ Boost/boost_calculate_date.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <string>
 
+#include <boost/data_time/gregorian/gregorian.hpp>
+
+using namespace boost::gregorian
 using namespace std;
 /*
-
-
-
+     //计算两个时间之间的间隔(以天为单位)
 */
 
 int main(int ac, char **av)
 {
+    string s1 = "1989-03-15";
+    date birthday(from_simple_string(s));
+    
+    date today = day_clock::local_day();/*获取系统当前时间*/
+    days days_alive = today - birthday;
+    cout << "已经出生了" << days_alive << " 天" << endl;
+    
+    // 也可以这样写
+    cout << "已经出生了" << days_alive.days() << " 天" << endl;
+
     return 0;
 }
